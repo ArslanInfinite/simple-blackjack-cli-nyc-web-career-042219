af709f6cf5db42104ab7c885f2472947329e572d
Arslan Ali <ali.arslan319@gmail.com>
2019-04-11 18:24:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,8 +32,16 @@ def initial_round
   card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+    if input == "h"
+      card_total += deal_card
+    elsif input == "s"
+      card_total
+    else 
+      invalid_command
+    end
 end
 
 def invalid_command
